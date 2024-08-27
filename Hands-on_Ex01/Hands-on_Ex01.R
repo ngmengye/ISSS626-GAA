f7538d7ae049b2952b85e24694350efbbb88e1f1
ngmengye <mengye1996@outlook.com>
2024-08-27 17:33:32 +0800

    Hands-on Exercise 1

---
+++ Hands-on_Ex01/Hands-on_Ex01.R
@@ -0,0 +1,5 @@
+#sf for importing, managing, and processing geospatial data
+#tidyverse for performing data science tasks such as importing
+#wrangling and visualizing data
+
+pacman::p_load(sf,tidyverse)
